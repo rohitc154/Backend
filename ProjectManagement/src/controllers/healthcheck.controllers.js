78bba79ecfa4820415fbeff1c1ab3b6ac1c9d216
Rohit Kumar <123806062+rohitc154@users.noreply.github.com>
2025-09-17 21:00:48 +0530

    Async Handler

--- ProjectManagement/src/controllers/healthcheck.controllers.js
+++ ProjectManagement/src/controllers/healthcheck.controllers.js
@@ -1,11 +1,20 @@
 import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthCheck = (req, res) => {
+/*
+const healthCheck = async (req, res, next) => {
   try {
+    const user = await getUserFromDB();
     res
       .status(200)
       .json(new ApiResponse(200, { message: "Server is Running !" }));
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 };
+*/
 
+const healthCheck = asyncHandler(async (req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: "Server is Up !" }));
+});
 export { healthCheck };
